9cb515bb4b3b965c4847d99d3955ca3df4105e1c
abarg12 <aidan.barg@gmail.com>
2022-12-03 16:27:24 -0500

    game logic for player collisions and score calculations

--- game_logic.c
+++ game_logic.c
@@ -18,12 +18,18 @@ void get_new_coords(Game g, PlayerPhysics *old_coords, PlayerPhysics *new_coords
 
 // return 1 if there's a wall, else 0
 int check_if_wall(Game g, int x, int y);
-int check_player_overlap(Game g, Player curr_p);        
 
+void check_player_collisions(Game g, PlayerPhysics *old_coords, PlayerPhysics *new_coords);
+void calculate_scores(Game g);
 
-void calculate_scores(Game g) {
+// for all three collision helpers below,
+// return 0 means no collision
+// return 1 means some collision
+int check_direct_overlap(PlayerPhysics *coords, int playerA, int playerB);
+//int check_partial_overlap();
+int check_passthrough(PlayerPhysics *old_coords, PlayerPhysics *new_coords, int playerA, int playerB);
 
-}
+Player get_nth_active_player(Game g, int n);
 
 void load_map(char *file_name, Game g) {
     FILE *fptr = fopen(file_name, "rb");
@@ -70,6 +76,7 @@ void update(Game g) {
     // update coords and take walls into consideration
     get_new_coords(g, old_coords, new_coords); 
 
+    check_player_collisions(g, old_coords, new_coords);        
 
     int player_number = 0;
     Player curr_player = g->players_head;
@@ -79,23 +86,6 @@ void update(Game g) {
             continue;     
         }
         
-
-            //TODO: check for collisions between player characters
-            /////////////////// begin Construction ZONE
-
-
-
-        // TODO: get the minotaur as g->active_p_head to update
-        //       the score when they get a player
-
-        //check_player_overlap(g, curr_player);        
-
-
-
-
-            /////////////////////// end of construction zone
-            
-            
         curr_player->phys.x = new_coords[player_number].x;
         curr_player->phys.y = new_coords[player_number].y;
         curr_player->phys.d = new_coords[player_number].d;
@@ -210,6 +200,201 @@ int check_if_wall(Game g, int x, int y) {
     return false;
 }
 
-int check_player_overlap(Game g, Player curr_p) {
+void check_player_collisions(Game g, PlayerPhysics *old_coords, PlayerPhysics *new_coords) {
+    assert(old_coords != NULL && new_coords != NULL);
+
+    Player p = g->players_head;
+    int n = 0;
+    while (p != NULL) {
+        if (p->player_state != PLAYING) {
+            p = p->next;
+            continue;
+        }
+        assert(p->phys.x > 0 && p->phys.x < MWIDTH && p->phys.y > 0 && p->phys.y < MHEIGHT);
+
+        // check against all other player positions to see if there's overlap
+        int i;
+        for (i = n + 1; i < g->num_active_players; i++) {
+            // direct overlap (same new coord positions)
+            if (check_direct_overlap(new_coords, n, i)) {
+                fprintf(stderr, "direct overlap detected\n");
+                
+                // have to be careful to cover case when one player is 'still' i.e.
+                // running into a wall, and if they are, then the other player has
+                // to be the one who gets reverted to their old position
+                if ((new_coords[n].x == old_coords[n].x) && (new_coords[n].y == old_coords[n].y)) {
+                    new_coords[i].x = old_coords[i].x;
+                    new_coords[i].y = old_coords[i].y;
+                } else {
+                    new_coords[n].x = old_coords[n].x;
+                    new_coords[n].y = old_coords[n].y;
+                }
+
+
+                // give the minotaur a score increase if they were part of collision
+                // and mark the player as 'collided_with'
+                if (n == 0 || i == 0) {
+                    Player mino = g->active_p_head;
+                    assert(mino != NULL);
+                    mino->score = mino->score + 1;
+
+                    if (n == 0) {
+                        Player human = get_nth_active_player(g, i);
+                        assert(human != NULL);
+                        human->collided_with = true;
+                    } else {
+                        Player human = get_nth_active_player(g, n);
+                        assert(human != NULL);
+                        human->collided_with = true;
+                    }
+                }
+
+
+                // restart the while loop to verify new collision correction
+                n = 0;
+                p = g->players_head;
+                continue;
+            }
+            
+            /*** scrapped the idea of partial overlap for now
+            // partial overlap (clients have width=2, so partial overlap is possible)
+            if (check_partial_overlap(new_coords, n, i)) {
+                fprintf(stderr, "partial overlap detected\n");
+                    
+            }
+            ***/
+            
+            // pass-through overlap (passed through each other)
+            if (check_passthrough(old_coords, new_coords, n, i)) {
+                fprintf(stderr, "passthrough detected\n");
+                new_coords[i].x = old_coords[i].x;
+                new_coords[i].y = old_coords[i].y;
+                new_coords[n].x = old_coords[n].x;
+                new_coords[n].y = old_coords[n].y;
+                
+
+                // give the minotaur a score increase if they were part of collision
+                if (n == 0 || i == 0) {
+                    Player mino = g->active_p_head;
+                    assert(mino != NULL);
+                    mino->score = mino->score + 1;
+
+                    if (n == 0) {
+                        Player human = get_nth_active_player(g, i);
+                        assert(human != NULL);
+                        human->collided_with = true;
+                    } else {
+                        Player human = get_nth_active_player(g, n);
+                        assert(human != NULL);
+                        human->collided_with = true;
+                    }
+                }
+
+
+                // restart the while loop to verify new collision correction
+                n = 0;
+                p = g->players_head;
+                continue;
+            }
+
+        }
+
+        
+        n = n + 1;
+        p = p->next;
+    }           
+}
+
+int check_passthrough(PlayerPhysics *old_coords, PlayerPhysics *new_coords, int playerA, int playerB) {
+    int a = playerA;
+    int b = playerB;
+
+    // first check to make sure both players are moving in opposite directions, this is
+    // the only case when a passthrough can happen
+    // use modmath to get the opposite directions (up = 0, right = 1, down = 2, left = 3)
+    if (((new_coords[a].d + 2) % 4) == new_coords[b].d) {
+
+        // now that we know they're moving in opposite directions, we can check whether
+        // they passed through each other
+        if ((old_coords[a].x == new_coords[b].x) && (old_coords[a].y == new_coords[b].y)) {
+            return 1;
+        }
+    }
+    
     return 0;
+}
+
+
+int check_direct_overlap(PlayerPhysics *coords, int playerA, int playerB) {
+    int a = playerA;
+    int b = playerB;
+
+    assert(coords[a].x > 0 && coords[a].x < MWIDTH && coords[a].y > 0 && coords[a].y < MHEIGHT);
+    assert(coords[b].x > 0 && coords[b].x < MWIDTH && coords[b].y > 0 && coords[b].y < MHEIGHT);
+
+    if ((coords[a].x == coords[b].x) && (coords[a].y == coords[b].y)) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+/**** partial overlap seems to be very complex, our solution is to make all player
+ **** movement L and R be in even-numbered squares so that the width-2 aspect of
+ **** player icons never leads to partial overlaps 
+ ****
+int check_partial_overlap(PlayerPhysics *coords, int playerA, int playerB) {
+    int a = playerA;
+    int b = playerB;
+    
+    assert(coords[a].x > 0 && coords[a].x < MWIDTH && coords[a].y > 0 && coords[a].y < MHEIGHT);
+    assert(coords[b].x > 0 && coords[b].x < MWIDTH && coords[b].y > 0 && coords[b].y < MHEIGHT);
+
+    if (((coords[a].x + 1) == coords[b].x) && (coords[a].y == coords[b].y)) {
+        return 1;
+    }
+
+    if ((coords[a].x  == (coords[b].x + 1)) && (coords[a].y == coords[b].y)) {
+        return 1;
+    }
+}
+****/
+
+
+void calculate_scores(Game g) {
+    Player p = g->players_head;
+    int n = 0;
+    while (p != NULL) {
+        if (p->player_state != PLAYING) {
+            p = p->next;
+            continue;
+        }
+
+        if ((n != 0) && (!p->collided_with)) {
+            p->score = p->score + 1;
+        }
+
+        p = p->next;
+        n = n + 1;
+    }
+}
+
+Player get_nth_active_player(Game g, int n) {
+    // 0-indexed
+    assert(n < g->num_active_players);
+
+    Player p = g->players_head;
+    int i = 0;
+    while (p != NULL) {
+        if (p->player_state != PLAYING) {
+            p = p->next;
+            continue;
+        }
+        if (i == n) {
+            return p;
+        }
+        p = p->next;
+        i = i + 1;
+    }
 }
